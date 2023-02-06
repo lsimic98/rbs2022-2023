1bddff67c319baa009a49adc9ea7cd649ed85abb
Luka Simic <61749939+lsimic98@users.noreply.github.com>
2023-02-06 22:35:48 +0100

    Dodata anotacija PreAuthorize za permisiju VIEW_PERSONS_LIST u klasi PersonsController.java

--- src/main/java/com/zuehlke/securesoftwaredevelopment/controller/PersonsController.java
+++ src/main/java/com/zuehlke/securesoftwaredevelopment/controller/PersonsController.java
@@ -8,6 +8,7 @@ import com.zuehlke.securesoftwaredevelopment.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -67,12 +68,14 @@ public class PersonsController {
     }
 
     @GetMapping("/persons")
+    @PreAuthorize("hasAuthority('VIEW_PERSONS_LIST')")
     public String persons(Model model) {
         model.addAttribute("persons", personRepository.getAll());
         return "persons";
     }
 
     @GetMapping(value = "/persons/search", produces = "application/json")
+    @PreAuthorize("hasAuthority('VIEW_PERSONS_LIST')")
     @ResponseBody
     public List<Person> searchPersons(@RequestParam String searchTerm) throws SQLException {
         return personRepository.search(searchTerm);
