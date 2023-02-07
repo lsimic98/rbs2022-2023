ad4908cb5ed3f2beccba893228e885367cc7aea1
Luka Simic <61749939+lsimic98@users.noreply.github.com>
2023-02-07 12:58:21 +0100

    Dodate LOG poruke za izuzetak SQLException u klasi UserRepository.java

--- src/main/java/com/zuehlke/securesoftwaredevelopment/repository/UserRepository.java
+++ src/main/java/com/zuehlke/securesoftwaredevelopment/repository/UserRepository.java
@@ -34,7 +34,7 @@ public class UserRepository {
                 return new User(id, username1, password);
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            LOG.warn("Failed to find user {}", username, e);
         }
         return null;
     }
@@ -46,7 +46,7 @@ public class UserRepository {
              ResultSet rs = statement.executeQuery(query)) {
             return rs.next();
         } catch (SQLException e) {
-            e.printStackTrace();
+            LOG.warn("Failed to validate credentials for user {}", username, e);
         }
         return false;
     }
@@ -58,7 +58,7 @@ public class UserRepository {
         ) {
             statement.executeUpdate(query);
         } catch (SQLException e) {
-            e.printStackTrace();
+            LOG.warn("Failed to delete user {}", userId, e);
         }
     }
 }
