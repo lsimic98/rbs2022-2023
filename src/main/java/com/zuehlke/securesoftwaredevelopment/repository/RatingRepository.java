afc4322b893de20dae9f77b081216169ea221dec
Luka Simic <61749939+lsimic98@users.noreply.github.com>
2023-02-07 12:54:27 +0100

    Dodate LOG poruke za izuzetak SQLException u klasi RatingRepository.java

--- src/main/java/com/zuehlke/securesoftwaredevelopment/repository/RatingRepository.java
+++ src/main/java/com/zuehlke/securesoftwaredevelopment/repository/RatingRepository.java
@@ -46,7 +46,7 @@ public class RatingRepository {
                 preparedStatement.executeUpdate();
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+           LOG.warn("Failed to create or update rating for movie {}", rating.getMovieId(), e);
         }
     }
 
@@ -60,7 +60,7 @@ public class RatingRepository {
                 ratingList.add(new Rating(rs.getInt(1), rs.getInt(2), rs.getInt(3)));
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            LOG.warn("Failed to get all ratings for moive {}", movieId, e);
         }
         return ratingList;
     }
